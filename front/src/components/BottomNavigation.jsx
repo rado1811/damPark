f1c035bd1127d94cb0e203eac4d79d396ca73910
rado1811 <36564887+rado1811@users.noreply.github.com>
2018-06-28 10:18:18 +0200

    insert icons.svg  navBar

--- front/src/components/BottomNavigation.jsx
+++ front/src/components/BottomNavigation.jsx
@@ -6,7 +6,11 @@ import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
 import Icon from '@material-ui/core/Icon';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
-// import VanG from '../../public/images/vanG';
+import Smoke from '../images/smoke.svg';
+import Art from '../images/art.svg';
+import Love from '../images/love.svg';
+import Eat from '../images/eat.svg';
+
 
 
 const styles = {
@@ -31,14 +35,14 @@ class LabelBottomNavigation extends React.Component {
     return (
     <div>
     <Grid container xs={12} s={12}>
-      <BottomNavigation style={{marginTop:'100%'}} value={value} onChange={this.handleChange} className={classes.root}>
-        <BottomNavigationAction value="love" icon={<Icon><img src="https://image.flaticon.com/icons/svg/109/109577.svg" alt="logo_canabis"/></Icon>} />
-        <BottomNavigationAction value="eat" icon={<Icon><img src="https://image.flaticon.com/icons/svg/934/934334.svg" alt="logo_canabis"/></Icon>} />
+      <BottomNavigation style={{marginTop:'1%'}} value={value} onChange={this.handleChange} className={classes.root}>
+        <BottomNavigationAction value="smoke" icon={<Icon><img src={Smoke} alt="logo_canabis"/></Icon>} />
+        <BottomNavigationAction value="art" icon={<Icon><img src={Art} alt="logo_art"/></Icon>} />
         <Button style={{height:100}} variant="fab" color="inherit" aria-label="add" className={classes.button}>
           <Icon><img src="https://image.flaticon.com/icons/svg/188/188987.svg" alt="AddIcon"/></Icon>
         </Button>
-        <BottomNavigationAction value="smoke" icon={<Icon><img src="https://image.flaticon.com/icons/svg/771/771002.svg" alt="logo_canabis"/></Icon>} />
-        <BottomNavigationAction value="art" icon={<Icon><img src="https://image.flaticon.com/icons/svg/15/15654.svg" alt="logo_canabis"/></Icon>} />
+        <BottomNavigationAction value="love" icon={<Icon><img src={Love} alt="logo_love"/></Icon>} />
+        <BottomNavigationAction value="eat" icon={<Icon><img src={Eat} alt="logo_eat"/></Icon>} />
       </BottomNavigation>
     </Grid>
     </div>
