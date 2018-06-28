4e35c7bbe19e5268b367d2fbe60135e0d38b0241
rado1811 <36564887+rado1811@users.noreply.github.com>
2018-06-29 01:25:34 +0200

    modalGuide.css

--- front/src/components/AttractionCard.jsx
+++ front/src/components/AttractionCard.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography';
@@ -46,47 +46,41 @@ const styles = {
   },
 };
 
-class AttractionCard extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+const AttractionCard = (props) => {
+  const { classes } = props;
 
-  render() {
-    const { classes } = this.props;
+  return (
+    <div className={classes.root}>
+      <ExpansionPanel className={classes.firstpanel}>
+        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+          <Avatar
+            alt="ImageCard"
+            src={thesexpeepshow}
+            className={classes.bigAvatar}
+            label="HELLO"
+          />
+          <Typography className={classes.titleCard}>
+          Le bol de soupe géant
+          </Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails className={classes.heading}>
+          <Typography className={classes.textCard}>
+          Piscine en forme de bol à déjeuner géant dans laquelle petits et grands peuvent se prélasser. En fonction des saisons, la piscine change de couleur : verte-salade l’été et rouge-tomate l’hiver. En plus de la couleur, cette piscine dégage un léger parfum de vacances. Maillot de bain obligatoire. <br/> <br/> 
+                Ouverture :
+            <br />
+                Fermeture :
+            <br />
+                Profil :
+            <br />
+                Age :
+            <br />
+                Accessibilité :
+          </Typography>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+    </div>
+  );
+};
 
-    return (
-      <div className={classes.root}>
-        <ExpansionPanel className={classes.firstpanel}>
-          <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-            <Avatar
-              alt="ImageCard"
-              src={thesexpeepshow}
-              className={classes.bigAvatar}
-              label="HELLO"
-            />
-            <Typography className={classes.titleCard}>
-            Le bol de soupe géant
-            </Typography>
-          </ExpansionPanelSummary>
-          <ExpansionPanelDetails className={classes.heading}>
-            <Typography className={classes.textCard}>
-            Piscine en forme de bol à déjeuner géant dans laquelle petits et grands peuvent se prélasser. En fonction des saisons, la piscine change de couleur : verte-salade l’été et rouge-tomate l’hiver. En plus de la couleur, cette piscine dégage un léger parfum de vacances. Maillot de bain obligatoire. <br/> <br/> 
-                  Ouverture :
-              <br />
-                  Fermeture :
-              <br />
-                  Profil :
-              <br />
-                  Age :
-              <br />
-                  Accessibilité :
-            </Typography>
-          </ExpansionPanelDetails>
-        </ExpansionPanel>
-      </div>
-    );
-  }
-}
 
 export default withStyles(styles)(AttractionCard);
--- front/src/components/BottomNavigation.jsx
+++ front/src/components/BottomNavigation.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
 import Grid from '@material-ui/core/Grid';
@@ -10,18 +8,8 @@ import Smoke from '../images/smoke.svg';
 import Art from '../images/art.svg';
 import Love from '../images/love.svg';
 import Eat from '../images/eat.svg';
-import mata from '../images/mata.svg';
-import vincent from '../images/vincent.svg';
-import houten from '../images/houten.svg';
-import dave from '../images/dave.svg';
 import entrance from '../images/buttoniconecity.svg';
 import './modalGuide.css';
-
-const styles = {
-  root: {
-    width: 500,
-  },
-};
 
 class LabelBottomNavigation extends React.Component {
   constructor(props) {
@@ -38,7 +26,6 @@ class LabelBottomNavigation extends React.Component {
     fetch(`/api/theme/${opt}`)
       .then(res => res.json())
       .then(activites => this.setState({ activites : activites, centerButton: require("../images/"+ activites[0].image_guide+".svg")}));
-      console.log('1', this.state.activites);
   }
     
   handleChange = (event, value) => {
@@ -54,14 +41,12 @@ class LabelBottomNavigation extends React.Component {
   };
 
   render() {
-    const { classes } = this.props;
     const { value } = this.state;
-      // console.log('guide', this.state.activites[0])
 
     return (
     <div>
     <Grid container>
-      <BottomNavigation value={value} onChange={this.handleChange} className={classes.root}>
+      <BottomNavigation value={value} onChange={this.handleChange} width="100%">
         <BottomNavigationAction value="smoke" 
                                 style={{padding: '1px'}} 
                                 onClick={() => this.filterBy("coffee")} 
@@ -74,7 +59,6 @@ class LabelBottomNavigation extends React.Component {
                                 style={{marginTop:'-10%', paddingRight: '1px', paddingLeft: '1px'}}
                                 onClick={this.handleOpen} 
                                 icon={<img src={this.state.centerButton} alt="logo_guide" width="100px" height="auto"/>} />
-
         <BottomNavigationAction value="love" 
                                 style={{padding: '1px'}} 
                                 onClick={() => this.filterBy("love")} 
@@ -91,9 +75,9 @@ class LabelBottomNavigation extends React.Component {
           onClose={this.handleClose}
         >
           <div className='ModalGuide'>
-          <img src={this.state.centerButton} alt="logo_guide" width="100px" height="auto" padding="0 auto"/>
+          <img src={this.state.centerButton} alt="logo_guide" width="100px" height="auto"/>
             <Typography variant="title" id="modal-title">
-              Text in a modal
+              Welkom!
             </Typography>
             <Typography variant="subheading" id="simple-modal-description">
               Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
@@ -105,8 +89,4 @@ class LabelBottomNavigation extends React.Component {
   }
 }
 
-LabelBottomNavigation.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(styles)(LabelBottomNavigation);
+export default LabelBottomNavigation
