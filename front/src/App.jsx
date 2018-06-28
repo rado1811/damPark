6edbc97ef52d1338f35a0fb1872ba0847119b329
soner8 <maco.dou@hotmail.com>
2018-06-28 18:59:16 +0200

    test Orhan

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,13 +1,69 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
-import StyledMap from './components/Map/Map2';
+import BottomNavigation from './components/BottomNavigation';
 
-const App = () => (
-  <div>
-    <Switch>
-      <Route exact path="/" component={StyledMap} />
-    </Switch>
-  </div>
-);
+const demoFancyMapStyles = require('./style.json');
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+    };
+  }
+
+  componentDidMount() {
+    this.fetchWithTimeOut();
+
+    const myLatLng = { lat: 52.379189, lng: 4.898455 };
+    const iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
+    const icons = {
+      weed: {
+        icon: `${iconBase}parking_lot_maps.png`,
+      },
+      eat: {
+        icon: `${iconBase}library_maps.png`,
+      },
+      culture: {
+        icon: `${iconBase}info-i_maps.png`,
+      },
+    };
+    const map = new window.google.maps.Map(document.getElementById('map'), {
+      center: { lat: 52.379189, lng: 4.898455 },
+      zoom: 13,
+      styles: demoFancyMapStyles,
+      options: {
+        streetViewControl: false,
+        scaleControl: false,
+        mapTypeControl: false,
+        panControl: false,
+        zoomControl: false,
+        rotateControl: false,
+        fullscreenControl: false,
+      },
+    });
+    this.state.data.map((activites) => {
+      const marker = new window.google.maps.Marker({
+        lat: activites.LAT,
+        lng: activites.LNG,
+        map,
+      });
+    });
+    const marker = new window.google.maps.Marker({
+      position: myLatLng,
+      map,
+      title: 'Hello World!',
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div id="map" style={{ height: '90vh', width: '100vw' }} />
+        <BottomNavigation />
+      </div>
+    );
+  }
+}
 
 export default App;
